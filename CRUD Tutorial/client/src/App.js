2afd7edebc5cc5b3634dacbf4aeeb0d9790d5f91
catherinelarson <catherinelarson456@gmail.com>
2022-09-18 13:35:11 -0500

    initial commit

---
+++ CRUD Tutorial/client/src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import { useState } from "react";
+
+function App() {
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="information">
+      <label>Name</label>
+      <input 
+      type="text" 
+      onChange={
+        (event) => { setName(event.target.value);}
+        }></input>
+      <label>Age</label>
+      <input 
+      type="number"
+      onChange={
+        (event) => { setAge(event.target.value);}
+        }></input>
+      <label>Country</label>
+      <input 
+      type="text"
+      onChange={
+        (event) => { setCountry(event.target.value);}
+        }></input>
+      <label>Position</label>
+      <input 
+      type="text"
+      onChange={
+        (event) => { setPosition(event.target.value);}
+        }></input>
+      <label>Wage (annual):</label>
+      <input 
+      type="number"
+      onChange={
+        (event) => { setWage(event.target.value);}
+        }></input>
+      <button>Add Employee</button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
